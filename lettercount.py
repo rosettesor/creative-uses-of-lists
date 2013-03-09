4848b2617b4d2d18270abc00d9f6c420a6fbd3da
Hackbright Student <user@mini-001.(none)>
2013-03-08 17:39:53 -0800

    exercise05

---
+++ lettercount.py
@@ -0,0 +1,14 @@
+from sys import argv
+import string
+
+script, filename = argv
+alphabet = string.ascii_lowercase
+alphabet.split(" ")
+
+txt = open(filename)
+txt.read()
+
+counter = 0
+for i in txt.read(i):
+
+target.read('\n'.join(alphabet) + '\n')
